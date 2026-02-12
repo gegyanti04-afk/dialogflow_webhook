f5b14854f2e387172ad729783507e2260a1e2626
gegyanti04-afk <gegyanti04@gmail.com>
2026-02-12 11:58:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -6,13 +6,31 @@ app.use(bodyParser.json());
 
 const jadwal = {
   senin: [
-    { waktu: "07:00 - 08:00", mapel: "Matematika" },
-    { waktu: "08:00 - 09:00", mapel: "Bahasa Indonesia" }
+    { waktu: "08:10 - 10:10", mapel: "agama" },
+    { waktu: "10:40 - 15:00", mapel: "DDA" }
   ],
   selasa: [
-    { waktu: "07:00 - 08:00", mapel: "IPA" },
-    { waktu: "08:00 - 09:00", mapel: "IPS" }
-  ]
+    { waktu: "07:30 - 09:30", mapel: "pjok" },
+    { waktu: "09:30 - 11:20", mapel: "matematika" }
+    { waktu: "11:20 - 15:40", mapel: "ipas" }
+  ],
+  rabu: [
+    { waktu: "07:30 - 08:50", mapel: "sejarah" },
+    { waktu: "08:50 - 13:20", mapel: "DDA" }
+    { waktu: "13:40 - 15:00", mapel: "KKA"  }
+  ],
+  kamis: [
+    { waktu: "07:30 - 08:50", mapel: "bahasa indonesia" },
+    { waktu: "08:50 - 12:00", mapel: "informatika" }
+    { waktu: "12:00 - 15:00", mapel: "bahasa inggris" }
+  ],
+  jumat: [
+    { waktu: "08:10 - 09:30", mapel: "seni budaya" },
+    { waktu: "09:30 - 11:20", mapel: "pkn" }
+    { waktu: "11:20 - 12:40", mapel: "bahasa indonesia" }
+    { waktu: "12:40  - 14:20", mapel: "bahasa bali" }
+    { waktu: "14:20  - 15:40", mapel: "matematika" }
+  ],
 };
 
 app.post("/webhook", (req, res) => {
@@ -37,4 +55,3 @@ app.post("/webhook", (req, res) => {
 app.listen(3000, () => {
   console.log("Server berjalan di port 3000");
 });
-
